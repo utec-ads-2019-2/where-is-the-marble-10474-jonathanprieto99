5a74bf423dd49549faabad667e3300f2c1cb0081
Jonathan Prieto <jonathan.prieto@me.com>
2019-11-06 11:14:36 -0500

    Upload Files

---
+++ cmake-build-debug/CMakeFiles/where-is-the-marble-10474-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/where-is-the-marble-10474-jonathanprieto99.dir/main.cpp.o"
+  "where-is-the-marble-10474-jonathanprieto99"
+  "where-is-the-marble-10474-jonathanprieto99.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/where-is-the-marble-10474-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/where-is-the-marble-10474-jonathanprieto99.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for where-is-the-marble-10474-jonathanprieto99.
+# This may be replaced when dependencies are built.
